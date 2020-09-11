0f7de029aec68fcea34464ff25d12e523ad029ac
Alan Peña <lanazirot@gmail.com>
2020-09-11 13:48:35 -0500

    For loop to while loop

--- Tarea 3/src/Fibonacci.java
+++ Tarea 3/src/Fibonacci.java
@@ -1,25 +1,11 @@
 public class Fibonacci{ 
-
-    public static int fib(int n){
-        if(n<=0)return 0;
-        if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
-    }
-
     public static void main(String[] args) {
-        
             
-            System.out.println("Recursivo fib(18) = "+fib(18));
-
-            int fib =0;
-            int aux =1;
-            System.out.println("Ciclico:");
-            for (int i = 0; i < 18; i++) {
-                fib+=aux;
+            int fib =0, aux=1, i=0;
+            while(i++<=10){
                 System.out.println(fib);
+                fib+=aux;
                 aux = fib - aux;
             }
-
     }
-
 }
