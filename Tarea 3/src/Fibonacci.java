086701d70825df49f016f058fd88e58eff138a5f
Alan Peña <lanazirot@gmail.com>
2020-09-11 13:51:39 -0500

    Changed fibo length

--- Tarea 3/src/Fibonacci.java
+++ Tarea 3/src/Fibonacci.java
@@ -2,7 +2,7 @@ public class Fibonacci{
     public static void main(String[] args) {
             
             int fib =0, aux=1, i=0;
-            while(i++<=10){
+            while(i++<=18){
                 System.out.println(fib);
                 fib+=aux;
                 aux = fib - aux;
